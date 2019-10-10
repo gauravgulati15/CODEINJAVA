d97b5005e0f388875276a1d21326423a51a9ff54
gauravgulati15 <56128284+gauravgulati15@users.noreply.github.com>
2019-10-11 00:00:23 +0530

    Add files via upload

---
+++ b9.java
@@ -0,0 +1,30 @@
+package test3;
+
+public class b9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         // for(int k=0;k<7;++k)
+          
+        	  for(int i=0;i<3;++i)
+        	  {
+        		  for(int j=i+1;j>0;--j)
+        		  {
+        			  System.out.print("*");
+        		  }
+        		  
+        		  for(int j=4;j>0;--j)
+        		  {
+        			  System.out.print(" ");
+        		  }
+        		  for(int j=1;j<3;++j)
+        		  {
+        			  System.out.print("*");
+        		  }
+        		  
+        		  System.out.println("");
+        	  }
+          
+	}
+
+}
